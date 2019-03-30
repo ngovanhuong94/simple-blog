f3f8fbd631537a16ebea5ead2ee2716d0227cbc7
ngovanhuong94 <ngovanhuong21994@gmail.com>
2019-03-30 18:42:28 +0300

    edit post get method

--- middleware/index.js
+++ middleware/index.js
@@ -1,3 +1,5 @@
+const Post = require('../models/Post')
+
 exports.isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) {
         next()
@@ -7,3 +9,36 @@ exports.isLoggedIn = (req, res, next) => {
     }
 }
 
+// check ownership
+exports.isOwnerPost = async (req, res, next) => {
+    if (req.isAuthenticated()) {
+        try {
+            // find post by id
+            const post = await Post.findById(req.params.id)
+
+            if (!post) {
+                // if not found the post
+                req.flash('error', 'Post not found')
+                return res.redirect('back')
+            } else {
+                // check ownership
+                console.log(req.user._id)
+                console.log(post.userId)
+                if (post.userId.equals(req.user._id)) {
+                    // same ids
+                    next()
+                } else {
+                    // not same ids
+                    req.flash('error', "You don't have permission")
+                    return res.redirect('back')
+                }
+            }
+        } catch (err) {
+            req.flash('error', 'Something went wrong')
+            return res.redirect('back')
+        }
+    } else {
+        req.flash('error', 'You must be logged in')
+        res.redirect('back')
+    }
+}
--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { isLoggedIn } = require('../middleware')
+const { isLoggedIn, isOwnerPost } = require('../middleware')
 const Post = require('../models/Post')
 
 router.get('/new', isLoggedIn, (req, res) => {
@@ -21,5 +21,25 @@ router.post('/new', isLoggedIn, async (req, res) => {
         return res.render('newpost', { error: 'Something went wrong'})
     }
 })
+
+
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        return res.render('showpost', { post: post })
+    } catch (err) {
+        return res.redirect('/')
+    }
+})
+
+router.get('/:id/edit', isOwnerPost,async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        return res.render('editpost', { post: post })
+    } catch (err) {
+        return res.redirect('/')
+    }
+})
+
 
 module.exports = router 
